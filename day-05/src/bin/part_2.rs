70c1528bde99b17ceb42dc7102da1df90ac11586
Pedro Soares <pmbs.123@gmail.com>
2023-12-06 10:59:30 +0100

    Day05: Completed :white_check_mark:

--- day-05/src/bin/part_2.rs
+++ day-05/src/bin/part_2.rs
@@ -1,6 +1,4 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
-
+use std::{cmp::min, collections::HashSet};
 fn main() {
     let input = include_str!("./input.txt");
     let output = part_2(input);
@@ -8,97 +6,195 @@ fn main() {
 }
 
 #[derive(Debug, Clone)]
-struct PartNumber {
-    number: String,
-    attached_gears: HashSet<(usize, usize)>,
+struct Seed {
+    start_range: u64,
+    offset: u64,
 }
 
-fn attached_gears(matrix: &Vec<Vec<char>>, row: usize, col: usize) -> Vec<(usize, usize)> {
-    let mut gears = Vec::<(usize, usize)>::new();
+#[derive(Debug, Clone)]
+struct Almanac {
+    seeds: Vec<Seed>,
+    seed_to_soil: Vec<MapDescription>,
+    soil_to_fert: Vec<MapDescription>,
+    fert_to_water: Vec<MapDescription>,
+    water_to_light: Vec<MapDescription>,
+    light_to_temp: Vec<MapDescription>,
+    temp_to_hum: Vec<MapDescription>,
+    hum_to_loc: Vec<MapDescription>,
+}
 
-    let rows = matrix.len();
-    let cols = matrix[0].len();
+impl Almanac {
+    fn new() -> Self {
+        Almanac {
+            seeds: Vec::new(),
+            seed_to_soil: Vec::new(),
+            soil_to_fert: Vec::new(),
+            fert_to_water: Vec::new(),
+            water_to_light: Vec::new(),
+            light_to_temp: Vec::new(),
+            temp_to_hum: Vec::new(),
+            hum_to_loc: Vec::new(),
+        }
+    }
 
-    for x in 0..3 {
-        for y in 0..3 {
-            let ni = row as i32 + x - 1;
-            let nj = col as i32 + y - 1;
+    fn create(&mut self, input: &str) {
+        let mut lines = input.lines();
+        self.seeds = parse_seeds(lines.nth(0).unwrap());
 
-            if x == 1 && y == 1 {
+        let mut current_map: u64 = 0;
+        let mut maps: Vec<&str> = Vec::new();
+
+        for (i, line) in lines.enumerate() {
+            if i < 1 {
                 continue;
             }
 
-            if ni >= 0 && ni < rows as i32 && nj >= 0 && nj < cols as i32 {
-                let c = matrix[ni as usize][nj as usize];
-                if c == '*' {
-                    gears.push((ni as usize, nj as usize));
-                }
+            // Beginning of map
+            if line.contains("map") {
+                maps = Vec::new();
+                continue;
             }
+
+            // End of map
+            if line == "" {
+                match current_map {
+                    0 => self.seed_to_soil = parse_map(maps.clone()),
+                    1 => self.soil_to_fert = parse_map(maps.clone()),
+                    2 => self.fert_to_water = parse_map(maps.clone()),
+                    3 => self.water_to_light = parse_map(maps.clone()),
+                    4 => self.light_to_temp = parse_map(maps.clone()),
+                    5 => self.temp_to_hum = parse_map(maps.clone()),
+                    6 => self.hum_to_loc = parse_map(maps.clone()),
+                    _ => println!("Something went wrong! Only expecting 7 maps."),
+                }
+                current_map += 1;
+
+                continue;
+            }
+            maps.push(line);
         }
     }
 
-    gears
+    fn soil_to_location(&self, seed: u64) -> u64 {
+        let mut location = seed;
+
+        for map in self.seed_to_soil.clone() {
+            if location >= map.source_start && location < map.source_start + map.offset {
+                location = map.target_start + location - map.source_start;
+                break;
+            }
+        }
+        for map in self.soil_to_fert.clone() {
+            if location >= map.source_start && location < map.source_start + map.offset {
+                location = map.target_start + location - map.source_start;
+                break;
+            }
+        }
+        for map in self.fert_to_water.clone() {
+            if location >= map.source_start && location < map.source_start + map.offset {
+                location = map.target_start + location - map.source_start;
+                break;
+            }
+        }
+        for map in self.water_to_light.clone() {
+            if location >= map.source_start && location < map.source_start + map.offset {
+                location = map.target_start + location - map.source_start;
+                break;
+            }
+        }
+        for map in self.light_to_temp.clone() {
+            if location >= map.source_start && location < map.source_start + map.offset {
+                location = map.target_start + location - map.source_start;
+                break;
+            }
+        }
+        for map in self.temp_to_hum.clone() {
+            if location >= map.source_start && location < map.source_start + map.offset {
+                location = map.target_start + location - map.source_start;
+                break;
+            }
+        }
+
+        for map in self.hum_to_loc.clone() {
+            if location >= map.source_start && location < map.source_start + map.offset {
+                location = map.target_start + location - map.source_start;
+                break;
+            }
+        }
+
+        location
+    }
 }
 
-fn part_2(input: &str) -> u32 {
-    let mut matrix: Vec<Vec<char>> = Vec::new();
+#[derive(Debug, Clone)]
 
-    // Convert input to a matrix of chars
-    for line in input.lines() {
-        let chars = line.chars().collect::<Vec<char>>();
-        matrix.push(chars);
+struct MapDescription {
+    source_start: u64,
+    target_start: u64,
+    offset: u64,
+}
+
+fn parse_seeds(line: &str) -> Vec<Seed> {
+    let mut seeds: Vec<Seed> = Vec::new();
+    let seeds_raw: Vec<u64> = line
+        .split(": ")
+        .nth(1)
+        .unwrap()
+        .split(' ')
+        .map(|n| n.parse::<u64>().unwrap())
+        .collect();
+
+    for seed in (0..seeds_raw.len()).step_by(2) {
+        let start_range = seeds_raw[seed];
+        let offset = seeds_raw[seed + 1];
+        let seed = Seed {
+            start_range,
+            offset,
+        };
+        seeds.push(seed);
     }
 
-    let mut parts = Vec::<PartNumber>::new();
-    let mut current_number = PartNumber {
-        number: String::new(),
-        attached_gears: HashSet::new(),
-    };
+    seeds
+}
 
-    let mut gears_map = HashMap::<(usize, usize), Vec<u32>>::new();
+fn parse_map_description(line: &str) -> MapDescription {
+    let desc = line
+        .split(' ')
+        .map(|n| n.parse::<u64>().expect("A number"))
+        .collect::<Vec<u64>>();
 
-    for (row, line) in matrix.iter().enumerate() {
-        for (col, c) in line.iter().enumerate() {
-            if c.is_numeric() {
-                current_number.number.push(*c);
-                let gears = attached_gears(&matrix, row, col);
-                current_number.attached_gears.extend(gears);
-            } else {
-                if current_number.attached_gears.len() > 0 {
-                    parts.push(current_number.clone());
-                    for gear in current_number.attached_gears.iter() {
-                        let gear_numbers = gears_map.entry(*gear).or_insert(Vec::<u32>::new());
-                        gear_numbers.push(
-                            current_number
-                                .number
-                                .parse::<u32>()
-                                .expect("Expected an integer"),
-                        );
-                    }
-                }
-                current_number.number = String::new();
-                current_number.attached_gears = HashSet::new();
-            }
+    MapDescription {
+        source_start: (*desc.get(1).unwrap()),
+        target_start: (*desc.get(0).unwrap()),
+        offset: (*desc.get(2).unwrap()),
+    }
+}
 
-            if col == line.len() - 1 {
-                if current_number.attached_gears.len() > 0 {
-                    parts.push(current_number.clone());
-                }
-                current_number.number = String::new();
-                current_number.attached_gears = HashSet::new();
-            }
+fn parse_map(maps: Vec<&str>) -> Vec<MapDescription> {
+    maps.iter()
+        .map(|line| parse_map_description(line))
+        .collect()
+}
+
+fn part_2(input: &str) -> u64 {
+    let mut almanac = Almanac::new();
+    almanac.create(input);
+
+    let mut min_: u64 = 999999999999999;
+
+    for seed in almanac.seeds.clone() {
+        println!("Processing one range");
+
+        let range = seed.start_range..seed.start_range + seed.offset - 1;
+
+        for i in range {
+            min_ = min(almanac.soil_to_location(i), min_);
         }
     }
 
-    gears_map
-        .iter()
-        .map(|(_, v)| {
-            if v.len() == 1 {
-                return 0;
-            }
-            v.iter().product()
-        })
-        .sum::<u32>()
+    // for seed in seeds {}
+
+    min_
 }
 
 #[cfg(test)]
@@ -107,17 +203,42 @@ mod tests {
 
     #[test]
     fn correct_output() {
-        let input = "467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..";
+        let input = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+
+";
         let result = part_2(input);
-        assert_eq!(result, 467835)
+        assert_eq!(result, 46)
     }
 }
