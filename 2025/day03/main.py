bff550f787caaa7083ed1c269c4ea0ce49b39d5e
Pedro Soares <pmbs.123@gmail.com>
2025-12-03 22:55:05 +0100

    2025 day03 part 2

--- 2025/day03/main.py
+++ 2025/day03/main.py
@@ -13,24 +13,43 @@ def parse(puzzle_input: str):
     return banks
 
 
-def part1(banks: list[list[str]]):
+def pick_k_digits(bank: str, size: int) -> str:
+    print("bank: ", bank)
+    print("len: ", len(bank))
+    to_remove = len(bank) - size
+    stack = []
+
+    for digit in bank:
+        print("outer", stack)
+        while to_remove and stack and stack[-1] < digit:
+            print("inner", stack, "size:", to_remove)
+            stack.pop()
+            to_remove -= 1
+        stack.append(digit)
+
+    return "".join(stack[:size])
+
+
+def part1(banks: list[str]):
     """Solve part 1."""
     total_joltage = 0
 
     for bank in banks:
-        max_joltage = 0
-        for i in range(len(bank) - 1):
-            for j in range(i + 1, len(bank)):
-                joltage = int(bank[i] + bank[j])
-                if joltage > max_joltage:
-                    max_joltage = joltage
-        total_joltage += max_joltage
+        v = pick_k_digits(bank, 2)
+        total_joltage += int(v)
 
     return str(total_joltage)
 
 
-def part2(data):
+def part2(banks):
     """Solve part 2."""
+    total_joltage = 0
+
+    for bank in banks:
+        v = pick_k_digits(bank, 12)
+        total_joltage += int(v)
+
+    return str(total_joltage)
 
 
 def solve(puzzle_input):
