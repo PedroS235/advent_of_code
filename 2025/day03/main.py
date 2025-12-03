a40d2b97c7713a9013acf0b9f361dbb60c5113bc
Pedro Soares <pmbs.123@gmail.com>
2025-12-03 16:04:24 +0100

    2025 day03 part 1

--- 2025/day03/main.py
+++ 2025/day03/main.py
@@ -6,10 +6,27 @@ import sys
 
 def parse(puzzle_input: str):
     """Parse input."""
+    banks = []
+    for bank in puzzle_input.splitlines():
+        banks.append(bank)
+
+    return banks
 
 
-def part1(data):
+def part1(banks: list[list[str]]):
     """Solve part 1."""
+    total_joltage = 0
+
+    for bank in banks:
+        max_joltage = 0
+        for i in range(len(bank) - 1):
+            for j in range(i + 1, len(bank)):
+                joltage = int(bank[i] + bank[j])
+                if joltage > max_joltage:
+                    max_joltage = joltage
+        total_joltage += max_joltage
+
+    return str(total_joltage)
 
 
 def part2(data):
