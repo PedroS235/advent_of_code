5f21cf3bee0da0aa7d0948d16e656a8d8c00d1b5
Pedro Soares <pmbs.123@gmail.com>
2025-12-05 15:46:55 +0100

    2025 day05 part1

--- 2025/day05/main.py
+++ 2025/day05/main.py
@@ -7,10 +7,39 @@ import sys
 def parse(puzzle_input: str):
     """Parse input."""
 
+    ranges = dict()
+    available = []
 
-def part1(data):
+    switch = False
+    for line in puzzle_input.splitlines():
+        if line == "":
+            switch = True
+            continue
+
+        if switch:
+            available.append(int(line))
+        else:
+            lower, upper = list(map(int, line.split("-")))
+
+            if ranges.get(lower):
+                if ranges[lower] < upper:
+                    ranges[lower] = upper
+            else:
+                ranges[lower] = upper
+    return ranges, available
+
+
+def part1(ranges: dict, available):
     """Solve part 1."""
 
+    # print(ranges)
+    # print(available)
+    fresh = 0
+    for id in available:
+        for key in ranges.keys():
+            if id >= key and id <= ranges[key]:
+                fresh += 1
+                break
 
 def part2(data):
     """Solve part 2."""
@@ -19,8 +48,8 @@ def part2(data):
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
     data = parse(puzzle_input)
-    solution1 = part1(data)
-    solution2 = part2(data)
+    solution1 = part1(*data)
+    solution2 = part2(*data)
 
     return solution1, solution2
 
