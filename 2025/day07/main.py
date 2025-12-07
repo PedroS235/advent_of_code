22b49ad0c3697ad16c6dd575c43cc1dce8259fba
Pedro Soares <pmbs.123@gmail.com>
2025-12-07 12:27:36 +0100

    2025 day07 part 1

--- 2025/day07/main.py
+++ 2025/day07/main.py
@@ -6,14 +6,57 @@ import sys
 
 def parse(puzzle_input: str):
     """Parse input."""
+    return puzzle_input.splitlines()
 
 
 def part1(data):
     """Solve part 1."""
 
+    start_col = 0
+
+    for j, col in enumerate(data[0]):
+        if col == "S":
+            start_col = j
+            break
+
+    splitters = set()
+    splitters.add(start_col)
+    total = 1
+
+    for row in data[1:]:
+        for j, col in enumerate(row):
+            if col == "^" and j in splitters:
+                total += 1
+                splitters.remove(j)
+                splitters.add(j - 1)
+                splitters.add(j + 1)
+
+    return str(total - 1)
+
+
+def find_timelines(data: list[list[str]], row: int, col: int):
+    if row == len(data):
+        return 1  # set([col])
+
+    # while row < len(data) - 1 and data[row][col] != "^":
+    #     row += 1
+
+    if data[row][col] != "^":
+        return find_timelines(data, row + 1, col)
+    right = find_timelines(data, row + 1, col + 1)
+    left = find_timelines(data, row + 1, col - 1)
+    # return set.union(right, left)
+    return right + left
+
 
 def part2(data):
     """Solve part 2."""
+    start_col = 0
+    for j, col in enumerate(data[0]):
+        if col == "S":
+            start_col = j
+            break
+    return str(find_timelines(data, 1, start_col))
 
 
 def solve(puzzle_input):
